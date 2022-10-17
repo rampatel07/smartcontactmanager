839ae4f69ccd29db5ef416cbf6c5d47bc3c9ddd6
rampatel07 <71453078+rampatel07@users.noreply.github.com>
2022-10-17 18:49:32 +0530

    First commit

---
+++ src/main/java/com/smart/dao/UserRepository.java
@@ -0,0 +1,12 @@
+package com.smart.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import com.smart.entites.Users;
+
+public interface UserRepository extends JpaRepository<Users,Integer> {
+    @Query("select u from Users u where u.email = :email")
+	public Users getUserByUserName(@Param("email") String email);
+}
